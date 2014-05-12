59b03b24982e71b0eeb5225d872b84e275428f8c
Sanket Navale <sanketnvl@gmail.com>
2014-05-12 12:28:30 +0530

    First change

---
+++ src/com/ex/Demo1.java
@@ -0,0 +1,13 @@
+package com.ex;
+
+public class Demo1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World 1");
+	}
+
+}
