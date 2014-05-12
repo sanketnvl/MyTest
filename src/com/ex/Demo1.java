9f6f63c180b476ab81fb6ad785f6507d2c8a74ec
Sanket Navale <sanketnvl@gmail.com>
2014-05-12 12:45:24 +0530

    Add new system line

--- src/com/ex/Demo1.java
+++ src/com/ex/Demo1.java
@@ -8,6 +8,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World 1");
+		System.out.println("Welcome 2");
 	}
 
 }
